b20d2ad9f465ffdf0af0c7d0d5f232998edd5138
Kael Paraguassu <kaelparaguassu@hotmail.com>
2025-07-01 17:13:19 -0300

    adicionando testes da estrutura de grafos

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -1,6 +1,6 @@
 import unittest
 
-from structures import Node, Graph  # Replace 'your_module' with the actual module name
+from graph_structures import Node, Graph  # Replace 'your_module' with the actual module name
 
 class TestNode(unittest.TestCase):
     def test_repr(self):
